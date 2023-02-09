6aa40ed39d5d321ce9ba0e36c305bcd22a9c3607
Luis cespedes <lxluxo23@gmail.com>
2023-02-08 21:10:08 -0300

    comentario

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def askquestion(question, engine):
 load_dotenv()
 if __name__ == "__main__":
     key = os.getenv("TOKEN")
-    print(Fore.GREEN + 'Busco pega \n' + Style.RESET_ALL)
+    # print(Fore.GREEN + 'Busco pega \n' + Style.RESET_ALL)
 
     if not key:
         print(
